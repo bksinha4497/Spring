f75b0549291c1bd66e3e3eddc1b60dfedc9cf648
Biswajit Kumar <bksinha4497@gmail.com>
2020-06-06 19:43:48 +0530

    spring boot Rest Controller example with project Lombok Getter and Setter

--- springboot-in-10-steps/src/main/java/com/tm/springboot/springbootin10steps/Book.java
+++ springboot-in-10-steps/src/main/java/com/tm/springboot/springbootin10steps/Book.java
@@ -1,7 +1,10 @@
 package com.tm.springboot.springbootin10steps;
 
-/*@Getter
-@Setter*/
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Book {
 
 	public Book(long id, String name, String author) {
@@ -12,31 +15,6 @@ public class Book {
 	}
 
 	long id;
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
 	String name;
 	String author;
 
